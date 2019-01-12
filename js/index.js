ddfbed3965f20d234d7f9996ab9df73b710ebefa
kanishka <kanishan14@gmail.com>
2019-01-13 00:05:44 +0530

    fix

--- js/index.js
+++ js/index.js
@@ -86,31 +86,6 @@ new ScrollMagic.Scene({
 
 
 
-new ScrollMagic.Scene({triggerElement: "#trigger2",})
-  .setClassToggle("#social_icons", "black") // add class toggle
-  .addTo(controller);
-
-  new ScrollMagic.Scene({triggerElement: "#trigger2",})
-  .setClassToggle("#hamburger-11", "black-menu") // add class toggle
-  .addTo(controller);
-
-
-new ScrollMagic.Scene({ triggerElement: "#trigger3" })
-  .setClassToggle("#social_icons", "white") // add class toggle
-  .addTo(controller);
-
-  new ScrollMagic.Scene({triggerElement: "#trigger3",})
-  .setClassToggle("#hamburger-11", "white-menu") // add class toggle
-  .addTo(controller);
-
-new ScrollMagic.Scene({ triggerElement: "#trigger4" })
-  .setClassToggle("#social_icons", "black2") // add class toggle
-  .addTo(controller);
-
-  new ScrollMagic.Scene({triggerElement: "#trigger4",})
-  .setClassToggle("#hamburger-11", "black-menu2") // add class toggle
-  .addTo(controller);
-
 
 // closing function for menu options
 
@@ -242,6 +217,10 @@ $win
       $(".line").css("background-color", "rgb(" + rgb2.join(",") + ")");
       $(".fa-icon").css("color", "rgb(" + rgb2.join(",") + ")");
     }
+    else {
+      $(".line").css("background-color", "");
+      $(".fa-icon").css("color", "");
+    }
 
     // setTimeout(function() {
     //   let l = Math.round(e.pageX);
@@ -283,3 +262,29 @@ $(function () {
     handles: "e"
   });
 });
+
+
+new ScrollMagic.Scene({triggerElement: "#trigger2",})
+  .setClassToggle("#social_icons", "black") // add class toggle
+  .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#trigger2",})
+  .setClassToggle("#hamburger-11", "black-menu") // add class toggle
+  .addTo(controller);
+
+
+new ScrollMagic.Scene({ triggerElement: "#trigger3" })
+  .setClassToggle("#social_icons", "white") // add class toggle
+  .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#trigger3",})
+  .setClassToggle("#hamburger-11", "white-menu") // add class toggle
+  .addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: "#trigger4" })
+  .setClassToggle("#social_icons", "black2") // add class toggle
+  .addTo(controller);
+
+  new ScrollMagic.Scene({triggerElement: "#trigger4",})
+  .setClassToggle("#hamburger-11", "black-menu2") // add class toggle
+  .addTo(controller);
